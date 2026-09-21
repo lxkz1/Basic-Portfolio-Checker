cc8653de08ab41bd27c0e1ee501b2a20d969d5fe
Luca Huang <luca.huang011@gmail.com>
2026-09-21 17:56:39 +0200

    Add multi-stock portfolio loop and share/price total value calc

--- yfinance_API_Project.py
+++ yfinance_API_Project.py
@@ -2,28 +2,44 @@ import yfinance
 
 
 def main():
-    usersstockvalue = getuserstockvalue()
-    sv = StockValueData(usersstockvalue)
-    print(f"The current stock value of {usersstockvalue} is: ${sv}")
+    print("Welcome to the Stock Value Calculator!")
+    print("This program will calculate the current value of your portfolio/stock holdings based on the ticker symbol and number of shares you own.")
+    numberofstocks = int(input("How many stocks do u own inside your portfolio? "))
+    if type(numberofstocks) != int:
+        print("please enter a valid number of stocks next time")
+    else:
+        for _ in range(numberofstocks):
+            ticker, shares = get_user_stock_value()
+            price = get_stock_price(ticker)
+            total_value = price * shares
+        print(f"The current value of your all of your shares of is: ${total_value:.2f}")
 
-def getuserstockvalue():
-    return input("Please Input the Ticker symbol you own: ")
-    
+
+def get_user_stock_value():
+    ticker = input("Please Input the Ticker symbol you own: ").strip().upper()
+    shares = get_share_amount()
+    return ticker, shares
 
 
-def StockValueData(userdata):
-    try: 
-        data = yfinance.Ticker(userdata).info
-        return data['regularMarketPrice']
-    except KeyError:
-        while True: 
+def get_share_amount():
+    while True:
+        shares = float(input("Please Input the amount of shares you own of this stock?: "))
+        if shares > 0:
+            return shares
+        else:
+            print("Please enter a valid number of shares greater than 0.")
+            get_share_amount()
+
+
+
+def get_stock_price(ticker):
+    while True:
+        try:
+            data = yfinance.Ticker(ticker).info
+            return data["regularMarketPrice"]
+        except KeyError:
             print("Invalid Ticker Symbol. Please try again.")
-            userdata = getuserstockvalue()
-            try:
-                data = yfinance.Ticker(userdata).info
-                return data['regularMarketPrice']
-            except KeyError:
-                continue
+            ticker = input("Please Input the Ticker symbol you own: ").strip().upper()
 
 
 if __name__ == "__main__":
